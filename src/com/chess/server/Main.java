15f8d2de5b57d7c614510c909f39180c6a5d37a4
simpu <id.simpu@gmail.com>
2019-09-22 03:34:36 +0530

    Updated ChessBoard

--- src/com/chess/server/Main.java
+++ src/com/chess/server/Main.java
@@ -1,7 +1,11 @@
 package com.chess.server;
 
+import com.chess.server.chessboard.ChessBoard;
+
 public class Main {
     public static void main(String[] args) {
+        ChessBoard board = new ChessBoard();
+        board.print();
         Console console = new Console();
         console.run();
     }
--- src/com/chess/server/chessboard/ChessBoard.java
+++ src/com/chess/server/chessboard/ChessBoard.java
@@ -21,42 +21,48 @@ public class ChessBoard {
             pos = new Position(i);
             pieceArray[i] = new Pawn(Color.WHITE, i, pos, PieceType.PAWN);
         }
-        pos=new Position(0);
-        pieceArray[0]=new Rook(Color.WHITE,0,pos,PieceType.ROOK);
-        pos=new Position(7);
-        pieceArray[7]=new Rook(Color.WHITE,7,pos,PieceType.ROOK);
-        pos=new Position(1);
-        pieceArray[1]=new Knight(Color.WHITE,1,pos,PieceType.KNIGHT);
-        pos=new Position(6);
-        pieceArray[6]=new Knight(Color.WHITE,6,pos,PieceType.KNIGHT);
-        pos=new Position(2);
-        pieceArray[2]=new Bishop(Color.WHITE,2,pos,PieceType.BISHOP);
-        pos=new Position(5);
-        pieceArray[5]=new Bishop(Color.WHITE,5,pos,PieceType.BISHOP);
-        pos=new Position(3);
-        pieceArray[3]=new King(Color.WHITE,3,pos,PieceType.KING);
-        pos=new Position(4);
-        pieceArray[4]=new Queen(Color.WHITE,4,pos,PieceType.QUEEN);
+        pos = new Position(0);
+        pieceArray[0] = new Rook(Color.WHITE, 0, pos, PieceType.ROOK);
+        pos = new Position(7);
+        pieceArray[7] = new Rook(Color.WHITE, 7, pos, PieceType.ROOK);
+        pos = new Position(1);
+        pieceArray[1] = new Knight(Color.WHITE, 1, pos, PieceType.KNIGHT);
+        pos = new Position(6);
+        pieceArray[6] = new Knight(Color.WHITE, 6, pos, PieceType.KNIGHT);
+        pos = new Position(2);
+        pieceArray[2] = new Bishop(Color.WHITE, 2, pos, PieceType.BISHOP);
+        pos = new Position(5);
+        pieceArray[5] = new Bishop(Color.WHITE, 5, pos, PieceType.BISHOP);
+        pos = new Position(3);
+        pieceArray[3] = new King(Color.WHITE, 3, pos, PieceType.KING);
+        pos = new Position(4);
+        pieceArray[4] = new Queen(Color.WHITE, 4, pos, PieceType.QUEEN);
+
+
+
+
         for (int i = 16; i < 24; i++) {
-            pos = new Position(i);
+            pos = new Position(i + 32);
             pieceArray[i] = new Pawn(Color.BLACK, i, pos, PieceType.PAWN);
         }
-        pos=new Position(24);
-        pieceArray[24]=new Rook(Color.BLACK,24,pos,PieceType.ROOK);
-        pos=new Position(31);
-        pieceArray[31]=new Rook(Color.BLACK,31,pos,PieceType.ROOK);
-        pos=new Position(25);
-        pieceArray[25]=new Knight(Color.BLACK,25,pos,PieceType.KNIGHT);
-        pos=new Position(30);
-        pieceArray[30]=new Knight(Color.BLACK,30,pos,PieceType.KNIGHT);
-        pos=new Position(26);
-        pieceArray[26]=new Bishop(Color.BLACK,26,pos,PieceType.BISHOP);
-        pos=new Position(29);
-        pieceArray[29]=new Bishop(Color.BLACK,29,pos,PieceType.BISHOP);
-        pos=new Position(27);
-        pieceArray[27]=new King(Color.BLACK,27,pos,PieceType.KING);
-        pos=new Position(28);
-        pieceArray[28]=new Queen(Color.BLACK,28,pos,PieceType.QUEEN);
+        pos = new Position(24 + 32);
+        pieceArray[24] = new Rook(Color.BLACK, 24, pos, PieceType.ROOK);
+        pos = new Position(31 + 32);
+        pieceArray[31] = new Rook(Color.BLACK, 31, pos, PieceType.ROOK);
+        pos = new Position(25 + 32);
+        pieceArray[25] = new Knight(Color.BLACK, 25, pos, PieceType.KNIGHT);
+        pos = new Position(30 + 32);
+        pieceArray[30] = new Knight(Color.BLACK, 30, pos, PieceType.KNIGHT);
+        pos = new Position(26 + 32);
+        pieceArray[26] = new Bishop(Color.BLACK, 26, pos, PieceType.BISHOP);
+        pos = new Position(29 + 32);
+        pieceArray[29] = new Bishop(Color.BLACK, 29, pos, PieceType.BISHOP);
+        pos = new Position(27 + 32);
+        pieceArray[27] = new King(Color.BLACK, 27, pos, PieceType.KING);
+        pos = new Position(28 + 32);
+        pieceArray[28] = new Queen(Color.BLACK, 28, pos, PieceType.QUEEN);
+
+
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 Position posn = new Position(i, j);
@@ -64,47 +70,52 @@ public class ChessBoard {
             }
         }
         Position posn = new Position(0);
-        boxArray[0][0].piece = new Pawn(Color.BLACK, 0, posn, PieceType.PAWN);
+        //boxArray[0][0].piece = new Pawn(Color.BLACK, 0, posn, PieceType.PAWN);
+        for (int i = 0; i < 32; i++) {
+            boxArray[pieceArray[i].boxID.x][pieceArray[i].boxID.y].piece = pieceArray[i];
+        }
     }
 
-    public Result giveResult(Move move,ChessBoard chessBoard) {
-        Result result = null;
-        if (move instanceof MoveTerminate) {
-            if (move.color == Color.BLACK)
-                result = new GameFinished(Color.WHITE);
-            else
-                result = new GameFinished(Color.BLACK);
-            return result;
-        } else {
-            if (move instanceof MoveNormal) {
-                int xs = ((MoveNormal) move).source.x;
-                int ys = ((MoveNormal) move).source.y;
-                if ((xs >= 8) || (xs < 0)) {
-                    result = new InvalidMove();
-                    return result;
-                } else if ((ys >= 8) || (ys < 0)) {
-                    result = new InvalidMove();
-                    return result;
-                }
-                int xd = ((MoveNormal) move).destination.x;
-                int yd = ((MoveNormal) move).destination.y;
-                if ((xd >= 8) || (xd < 0)) {
-                    result = new InvalidMove();
-                    return result;
-                } else if ((yd >= 8) || (yd < 0)) {
-                    result = new InvalidMove();
-                    return result;
-                }
-                if (((MoveNormal) move).source.equals((boxArray[xs][ys].piece.boxID))) {
-                    if (((MoveNormal) move).source.equals(pieceArray[((MoveNormal) move).pieceId].boxID)){
-                        if (chessBoard.pieceArray[((MoveNormal) move).pieceId].checkValid(((MoveNormal) move),chessBoard)){
-                            result=new GameFinished(Color.WHITE);
-                            return result;
-                        }
-                    }
-                }
+    public void print() {
+        for (int y = 7; y >= 0; y--) {
+            for (int x = 0; x < 8; x++) {
+                if (!boxArray[x][y].isEmpty()) {
+                    int i = boxArray[x][y].piece.getPieceID();
+                    System.out.print("[" + ((i / 10 == 0) ? "0" : "") + boxArray[x][y].piece.getPieceID() + "]");
+                } else
+                    System.out.print("[  ]");
             }
-            return result;
+            System.out.println();
         }
+    }
+
+    private Result evaluate(MoveTerminate move) {
+        return new GameFinished(move.color == Color.BLACK ? Color.WHITE : Color.BLACK);
+    }
+
+    private boolean checkBounds(Position pos) {
+        return pos.isDead();
+    }
+
+    private Result evaluate(MoveNormal move) {
+        int xs = move.source.x;
+        int ys = move.source.y;
+        if (checkBounds(move.source) || checkBounds(move.destination))
+            return new InvalidMove();
+        if (boxArray[move.source.x][move.source.y].isEmpty())
+            return new InvalidMove();
+        if (boxArray[move.source.x][move.source.y].piece.pieceID != move.pieceId)
+            return new InvalidMove();
+        if (!pieceArray[move.pieceId].checkValid(move, this))
+            return new InvalidMove();
+        return new GameFinished(Color.WHITE);
+    }
+
+    public Result evaluate(Move move) {
+        if (move instanceof MoveTerminate)
+            return evaluate((MoveTerminate) move);
+        if (move instanceof MoveNormal)
+            return evaluate((MoveNormal) move);
+        return new InvalidMove();
     }
 }
