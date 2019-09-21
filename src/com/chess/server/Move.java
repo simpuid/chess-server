293697094127865edc96b692af996107cef93836
simpu <id.simpu@gmail.com>
2019-09-21 22:04:13 +0530

    Added timeStamp on Move

--- src/com/chess/server/Move.java
+++ src/com/chess/server/Move.java
@@ -4,12 +4,14 @@ public class Move {
     public MoveType type;
     public int pieceId;
     public Position source;
-    public Position target;
+    public Position destination;
+    public int timeStamp;
 
-    Move(MoveType type,int pieceId,Position source,Position target){
+    Move(MoveType type,int pieceId,Position source,Position destination,int timeStamp){
         this.type = type;
         this.pieceId = pieceId;
         this.source = source;
-        this.target = target;
+        this.destination = destination;
+        this.timeStamp = timeStamp;
     }
 }
