05c5821a461c060b6ededefc686d26cd6b340ddc
simpu <id.simpu@gmail.com>
2019-09-21 22:40:36 +0530

    Added parser

--- src/com/chess/server/common/moves/Move.java
+++ src/com/chess/server/common/moves/Move.java
@@ -3,7 +3,6 @@ package com.chess.server.common.moves;
 import com.chess.server.common.Position;
 
 public class Move {
-    public MoveType type;
     public int pieceId;
     public Position source;
     public Position destination;
