750cee30255b7912ee47ce08e2e6e6a76297f5a3
simpu <id.simpu@gmail.com>
2019-09-22 05:16:08 +0530

    Updated Pawn

--- src/com/chess/server/Main.java
+++ src/com/chess/server/Main.java
@@ -1,12 +1,14 @@
 package com.chess.server;
 
 import com.chess.server.chessboard.ChessBoard;
+import com.chess.server.common.Position;
 
 public class Main {
     public static void main(String[] args) {
         ChessBoard board = new ChessBoard();
+        System.out.println(board.lineTest(new Position(2, 1), new Position(2, 6)));
         board.print();
         Console console = new Console();
-        console.run();
+        //console.run();
     }
 }
