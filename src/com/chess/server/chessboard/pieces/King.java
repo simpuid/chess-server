e16db595e3035860cbde780437e720ec842ba5b5
codersaiyan <prathameshkatkar2000@gmail.com>
2019-09-22 01:53:54 +0530

    Completed Pawn Class

--- src/com/chess/server/chessboard/pieces/King.java
+++ src/com/chess/server/chessboard/pieces/King.java
@@ -8,6 +8,7 @@ public class King extends Piece {
         this.pieceID = pieceID;
         this.boxID = boxID;
         this.type=type;
+        this.moveCount=0;
     }
 
     public boolean checkValid() {
--- src/com/chess/server/chessboard/pieces/Knight.java
+++ src/com/chess/server/chessboard/pieces/Knight.java
@@ -8,6 +8,7 @@ public class Knight extends Piece {
         this.pieceID = pieceID;
         this.boxID = boxID;
         this.type=type;
+        this.moveCount=0;
     }
 
     public boolean checkValid() {
