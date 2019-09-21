89a7bc466bd6f0086d1b9382802997b8b8393ff5
simpu <id.simpu@gmail.com>
2019-09-21 20:25:47 +0530

    Added Move Class

--- src/com/chess/server/Move.java
+++ src/com/chess/server/Move.java
@@ -2,4 +2,14 @@ package com.chess.server;
 
 public class Move {
     public MoveType type;
+    public int pieceId;
+    public Position source;
+    public Position target;
+
+    Move(MoveType type,int pieceId,Position source,Position target){
+        this.type = type;
+        this.pieceId = pieceId;
+        this.source = source;
+        this.target = target;
+    }
 }
