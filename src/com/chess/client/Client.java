57140fa29082fe497f11cb0bf52fed841e6a079b
simpu <id.simpu@gmail.com>
2019-09-26 23:58:51 +0530

    Adding client

---
+++ src/com/chess/client/Client.java
@@ -0,0 +1,11 @@
+package com.chess.client;
+
+public class Client {
+    public Client() {
+
+    }
+
+    void run() {
+
+    }
+}
--- src/com/chess/server/Server.java
+++ src/com/chess/server/Server.java
@@ -1,6 +1,7 @@
 package com.chess.server;
 
 import com.chess.server.chessboard.ChessBoard;
+import com.chess.server.common.Position;
 import com.chess.server.common.moves.Move;
 import com.chess.server.common.results.Result;
 
@@ -8,9 +9,17 @@ class Server {
     private IdGenerator idGenerator;
     public ChessBoard board;
 
-    Server(){
+    Server(int port) {
         idGenerator = new IdGenerator();
         board = new ChessBoard();
+    }
+
+    void run() {
+        ChessBoard board = new ChessBoard();
+        System.out.println(board.lineTest(new Position(2, 1), new Position(2, 6)));
+        board.print();
+        Console console = new Console();
+        console.run();
     }
 
     Result getResult(Move move) {
